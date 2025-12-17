eb8398c3a00f9ce33d580385d43e166d88c8c5de
Sai Kumar Karlapati <ksaikumar_csm206632@mgit.ac.in>
2025-12-17 12:43:00 +0530

    Initial Commit

---
+++ patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
@@ -0,0 +1,14 @@
+package com.pm.patientservice.repository;
+
+import com.pm.patientservice.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+    boolean existsByEmail(String email);
+//    “Does there exist any record with this email, but NOT this ID?”
+    boolean existsByEmailAndIdNot(String email,UUID id);
+}
